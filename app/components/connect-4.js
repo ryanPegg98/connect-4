d245092530856fd9f1e1b0ff731de1df058f6393
Ryan Pegg <ryan.luke.pegg@gmail.com>
2019-11-29 14:33:01 +0000

    Added Comments and Tidy Up:
    * Added more comments
    
    * Changed the variable names
    
    * Optimisation

--- app/components/connect-4.js
+++ app/components/connect-4.js
@@ -6,9 +6,11 @@ import Component from '@ember/component';
 */
 
 function deepClone(state){
+  // Initiate the new state which will be returned to the
   var new_state = [];
-  for(var idx = 0; idx < state.length; idx++){
-    new_state.push(state[idx].slice(0));
+  // Loop over each of the arrays which are acting as the X axis for the board
+  for(var x = 0; x < state.length; x++){
+    new_state.push(state[x].slice(0));
   }
   return new_state;
 }
@@ -116,95 +118,248 @@ function check_game_winner(state){
       }
     }
   }
-    // Initiall set it to true
-    var draw = true;
-    // Loop over all of the columns
-    for(var x = 0; x <= 6; x++){
-      // Loop over all of the rows
-      for(var y = 0; y <= 5; y++){
-        // If one does not have a value then the game is still in motion
-        if(!state[x][y]){
-          // Return undefined to the place the method was called
-          // This will initcate that the game is still active
-          return undefined;
-        }
+  // Loop over all of the columns
+  for(var x = 0; x <= 6; x++){
+    // Loop over all of the rows
+    for(var y = 0; y <= 5; y++){
+      // If one does not have a value then the game is still in motion
+      if(!state[x][y]){
+        // Return undefined to the place the method was called
+        // This will initcate that the game is still active
+        return undefined;
       }
     }
-    // Return a blank string. This will show it has been drawn.
-    return '';
+  }
+  // Return a blank string. This will show it has been drawn.
+  return '';
+}
+
+/*
+  These are the possible ways that the computer could win
+*/
+
+var patterns = [
+  {
+    pattern: [['p', 0, 1], ['p', 0, 1], ['p', 0, 1], ['p']],
+    score: 1000
+  },
+  {
+    pattern: [['p', 1, 0], ['p', 1, 0], ['p', 1, 0], ['p']],
+    score: 1000
+  },
+  {
+    pattern: [['p', 1, 1], ['p', 1, 1], ['p', 1, 1], ['p']],
+    score: 1000
+  },
+  {
+    pattern: [['p', 1, -1], ['p', 1, -1], ['p', 1, -1], ['p']],
+    score: 1000
+  },
+  {
+    pattern: [['p', 0, 1], ['p', 0, 1], ['p']],
+    score: 100
+  },
+  {
+    pattern: [['p', 1, 0], ['p', 1, 0], ['p']],
+    score: 100
+  },
+  {
+    pattern: [['p', 1, 1], ['p', 1, 1], ['p']],
+    score: 100
+  },
+  {
+    pattern: [['p', 1, -1], ['p', 1, -1], ['p']],
+    score: 100
+  },
+  {
+    pattern: [['p', 0, 1], ['p']],
+    score: 50
+  },
+  {
+    pattern: [['p', 1, 0], ['p']],
+    score: 50
+  },
+  {
+    pattern: [['p', 1, 1], ['p']],
+    score: 50
+  },
+  {
+    pattern: [['p', 1, -1], ['p']],
+    score: 50
+  }
+];
+
+
+/*
+  This fucntion will check for to see it there is a match in the current game board known as
+  state in the code
+
+  This method should return a boolean value (True or False)
+*/
+
+function match_pattern_at(state, pattern, player, x, y){
+  // Ensure the that X and Y are within the state
+  if( x >= 0 && x < state.length && y >= 0 && y < state[x].length ) {
+    var element = pattern[0]; // This will get the first element in the patern array
+    // If the first element is not occupied by the player then that pattern is not the right one
+    if(element[0] === 'p' && state[x][y] !== player){
+      // Return false as it does not match
+      return false;
+    } else if(element[0] === ' ' && state[x][y] !== undefined){ // Look to see if the slot has been occupied and is not meant to be
+      // Return false as this is also not a match
+      return false;
+    }
+    // Check that there is more than one element in the pattern
+    if(pattern.length > 1){
+      // this will reccusivley check the whole pattern
+      return match_pattern_at(state, pattern.slice(1), player, x + element[1], y + element[2]);
+    } else {
+      // This pattern is a match so true should be returned
+      return true;
+    }
+  }
+
+  // If all else fails return false
+  return false;
+}
+
+/*
+  This function will look at the list of winable patterns to try and get the computer to win
+  this will make it harder for the user
+*/
+
+function match_pattern(state, pattern, player){
+  // First loop over the whole X axis of the board, hence why x has been used as a variable name
+  for(var x = 0; x < state.length; x++){
+    // Then loop over all of the Y axis, which is the reason y is being used as the variable name
+    for(var y = 0; y < state[x].length; y++){
+      var matches = match_pattern_at(state, pattern, player, x, y);
+      // If there are any matches the match_pattern_at method will return true
+      if(matches){
+        return true;
+      }
+    }
+  }
+  // If nothing matches then return false
+  return false;
+}
+
+/*
+  This will improve the decisions made by the computer player using
+  a heuristic approch to calculate the best move
+*/
+
+function heuristic(state){
+  var score = 0; // The score of the move will need to be stored while the loop is running, this will outputted by the method
+  for(var i = 0; i < patterns.length; i++){ // the variable i has been used. This stores which iteration of the for loop it is.
+    if(match_pattern(state, patterns[i].pattern, 'yellow')){
+      score = score + patterns[i].score; // Add the paterns score to the score variable
+    }
+    if(match_pattern(state, patterns[i].pattern, 'red')){
+      score = score - patterns[i].score; // Remove the score from score variable
+    }
+  }
+
+  return score; // Return the integer that has been set in the score variable
 }
 
 /*
   This method will try and find the best move for the computer
   to make it harder for the player
+
+  This should return an array of moves
 */
 
 function minimax(state, limit, player){
+  // This will be the list of moves that the computer could take with the scores that they have
   var moves = [];
-
+  // If the limit is less than one then the computers predictions have reached the end of the line
   if(limit > 0){
-    for(var idx1 = 0; idx1 < 7; idx1++){
-      for(var idx2 = 0; idx2 < 6; idx2++){
-        if(state[idx1][idx2] === undefined){
+    // Loop over all of the X axis for the state, which is the reason x is being used as the variable
+    for(var x = 0; x < state.length; x++){
+      // Loop over the Y axis, which is why Y is being used as the variable name
+      for(var y = 0; y < state[x].length; y++){
+        // Check to see if the slot in the state is undefined
+        if(state[x][y] === undefined){
+          /*
+           Create the key value array for this being a potential move
+           set the X and Y co-ordinates, clone the state and set the score to 0
+          */
           var move = {
-            x: idx1,
-            y: idx2,
+            x: x,
+            y: y,
             state: deepClone(state),
             score: 0
           };
+          // In the cloned state set it to be the player defined int he
+          move.state[x][y] = player;
 
-          move.state[idx1][idx2] = player;
-
+          // If the limit is one or the there is a result from the check winner method fetch the score
           if(limit === 1 || check_game_winner(move.state) !== undefined) {
-            if(check_game_winner(move.state) !== undefined) {
-              var winner = check_game_winner(move.state);
-              if(winner === 'yellow') {
-                move.score = 1000;
-              } else if(winner === 'red') {
-                move.score = -1000;
-              }
-            }
-          } else {
+            // use the heuristic method to get the score
+            move.score = heuristic(move.state);
+          } else { // otherwise reccusivley generate a potential win by simulating the next turn
+            // Store the potential moves into its own array within the already defined key value array
             move.moves = minimax(move.state, limit - 1, player === 'red' ? 'yellow' : 'red');
+            // Keep the score undefined for now
             var score = undefined;
 
-            for(var idx3 = 0; idx3 < move.moves.length; idx3++){
+            // Loop over all of the moves that have been found, the variable here is m, standing for move
+            for(var m = 0; m < move.moves.length; m++){
               if (score === undefined){
-                score = move.moves[idx3].score;
+                // if the score is undefined this should be set the the score value from the move in the m position
+                score = move.moves[m].score;
               } else if(player === 'red') {
-                score = Math.max(score, move.moves[idx3].score);
+                // if the player is red (user) then select which is the biggest from the current score or the score from the move in the m position
+                score = Math.max(score, move.moves[m].score);
               } else if(player === 'yellow') {
-                score = Math.min(score, move.moves[idx3].score);
+                // However, if it is the computer then the smallest score will need to be selected
+                score = Math.min(score, move.moves[m].score);
               }
             }
-
+            // Set the moves score as the value that has been found and stored in the score variable
             move.score = score;
           }
 
+          // Append the move to the array of move moves that could be used
           moves.push(move);
         }
       }
     }
   }
 
+  // Time to return the list of moves for the computer to pick from
   return moves;
 }
 
-function computer_move(state){
-  var moves = minimax(state, 3, 'yellow');
-  var max_score = undefined;
-  var move = undefined;
+/*
+  This function will be used to identify where to place the computers counter.
 
-  for(var idx = 0; idx < moves.length; idx++){
-    if(max_score === undefined || moves[idx].score > max_score) {
-      max_score = moves[idx].score;
+  It will return the X and Y co-oridinates of where to place the counter
+*/
+
+function computer_move(state){
+  // Use the minimax function to get the moves that are available for the computer and store them in the move variable
+  // These are the moved that it will pick from
+  var moves = minimax(state, 3, 'yellow');
+  var move_score = undefined; // This will need to be undefined by default as it will keep moves score
+  var move = undefined; // This will store the co-ordinates for the next computer more and return them as the result
+
+  // Loop over each move. Using the variable m, which stands for move
+  for(var m = 0; m < moves.length; m++){
+    // If the max_score has not been filled in or the moves score is greater than the current score
+    if(move_score === undefined || moves[m].score > move_score) {
+      // Set the move_score to be the new current moves score
+      move_score = moves[m].score;
+      // Set the move to be the co-ordinates for this current move
       move = {
-        x: moves[idx].x,
-        y: moves[idx].y
+        x: moves[m].x,
+        y: moves[m].y
       }
     }
   }
 
+  // Pass the key value array that is stored in the move variable
   return move;
 }
 
@@ -345,22 +500,39 @@ export default Component.extend({
           // Increment that players move
           component.get('moves')['red'] = moveCounter + 1;
 
-          setTimeout(function(){
-            if(!component.get('winner') && !component.get('draw')){
+          // The computer should only play if there is a winner
+          if(!component.get('winner') && !component.get('draw')){
+            /*
+              The setTimeout will add some time to the computers move.
+              This will make it feel like they are playing a human
+            */
+            setTimeout(function(){
+              // Get the computers move that has been picked by the computer move method
               var move = computer_move(state);
+              // Get the number of moves to select the counter to move
+              // The computer will always be yellow
               moveCounter = component.get('moves')['yellow'];
+              // Set the board state to set the selected slot to be the computer player
               state[move.x][move.y] = 'yellow';
+              // Select the counter that will need to be moved on the grid
               counter = component.get('counters')['yellow'][moveCounter];
+              // Set the correct co-ordinates for the counter for the selected slot
               counter.x = (move.x * 60) + 30;
               counter.y = (move.y * 60) + 30;
+              // Make the counter visable
               counter.visible = true;
+              // Fade the counter in using the Tween package
               createjs.Tween.get(counter).to({alpha: 1}, 500);
+              // Make the sound of dropping when placing the counter
               createjs.Sound.play("dropping");
+              // Increment he move counter so that the next counter is selected
               component.get('moves')['yellow'] = moveCounter + 1;
+              // Update the stage to show a the counters that are being displayed
               component.get('stage').update();
+              // Check for the winner again incase the computer is the winner
               component.check_winner();
-            }
-          }, 500);
+            }, 500);
+          }
         }
       }
     }
@@ -397,14 +569,21 @@ export default Component.extend({
       if(this.get('playing')){
         var counters = this.get('counters');
         for(var idx = 0; idx < 21; idx++){
+          // This will animate the board to remove all of the counters
           createjs.Tween.get(counters.red[idx]).to({alpha: 0}, 500);
           createjs.Tween.get(counters.yellow[idx]).to({alpha: 0}, 500);
         }
+        // This will play the woosh sound that has been defined in the intial Method
+        // This will only play when reseting the the board
         createjs.Sound.play("woosh");
       }
 
-      // Set the state that will show which player has a counter in that area
-      // undefined is the row(y) and the arrays are the columns(x)
+      /*
+        Set the state that will show which player has a counter in that area
+        undefined is the row(y) and the arrays are the columns(x)
+
+        State is being used as this is the state of the board
+      */
       this.set('state', [
         [undefined, undefined, undefined, undefined, undefined, undefined],
         [undefined, undefined, undefined, undefined, undefined, undefined],
